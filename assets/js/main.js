3b28d417fbb977289762e5198ea1bb860ea46541
hande-ozen <ozn.hande@gmail.com>
2025-10-05 03:45:02 +0200

    first template, render pixels

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ const start = document.getElementById("page-home");
 const templates = document.getElementById("page-templates");
 const game = document.getElementById("page-artboard");
 const popUp = document.getElementById("pop-up");
+const pixelBox = document.getElementById("pixelBox");
 
 // Buttons
 
@@ -14,7 +15,94 @@ const btnPaletteLeft = document.getElementById("btnPaletteLeft");
 const btnPaletteRight = document.getElementById("btnPaletteRight");
 const thumbnails = document.getElementsByClassName("thumbnail");
 
-console.log(thumbnails);
+//Game Logic
+
+let artboardWidth = 0; // Will be set when the artboard is opened
+
+// ======================================================================= //
+
+const creeperConfig = {
+    width:8,
+    height:8,
+    colors:["#70C47A", "#C6FFA8", "#000000",]
+};
+
+const creeper = [
+    "#70C47A",
+    "#70C47A",
+    "#C6FFA8",
+    "#C6FFA8",
+    "#70C47A",
+    "#70C47A",
+    "#70C47A",
+    "#C6FFA8",
+
+    "#70C47A",
+    "#70C47A",
+    "#C6FFA8",
+    "#70C47A",
+    "#C6FFA8",
+    "#C6FFA8",
+    "#70C47A",
+    "#70C47A",
+
+    "#C6FFA8",
+    "#000000",
+    "#000000",
+    "#70C47A",
+    "#70C47A",
+    "#000000",
+    "#000000",
+    "#70C47A",
+
+    "#70C47A",
+    "#000000",
+    "#000000",
+    "#C6FFA8",
+    "#70C47A",
+    "#000000",
+    "#000000",
+    "#70C47A",
+
+    "#C6FFA8",
+    "#70C47A",
+    "#70C47A",
+    "#000000",
+    "#000000",
+    "#C6FFA8",
+    "#C6FFA8",
+    "#70C47A",
+
+    "#70C47A",
+    "#C6FFA8",
+    "#000000",
+    "#000000",
+    "#000000",
+    "#000000",
+    "#70C47A",
+    "#C6FFA8",
+
+    "#C6FFA8",
+    "#70C47A",
+    "#000000",
+    "#000000",
+    "#000000",
+    "#000000",
+    "#C6FFA8",
+    "#70C47A",
+
+    "#70C47A",
+    "#C6FFA8",
+    "#000000",
+    "#70C47A",
+    "#70C47A",
+    "#000000",
+    "#C6FFA8",
+    "#70C47A",
+]
+
+
+
 
 // ======================================================================= //
 
@@ -42,7 +130,24 @@ function startGame() {
 function openArtboard() {
     templates.classList.add("hidden");
     game.classList.remove("hidden");
+
+    artboardWidth = pixelBox.offsetWidth;
+    renderPixels();
+    
 }
+
+function renderPixels() {
+    // Ekrana pikselleri <div> olarak ekle
+    pixelBox.innerHTML = ""; // Temizle
+    for (let i = 0; i < creeper.length; i++) {
+        const pixel = document.createElement("div");
+        pixel.style.width = artboardWidth / creeperConfig.width + "px";
+        pixel.style.height = artboardWidth / creeperConfig.width + "px";
+        pixel.style.backgroundColor = creeper[i];
+        pixelBox.appendChild(pixel);
+    }
+}   
+
 
 function goBack() {
     game.classList.add("hidden");
