cb6ec258d58b63dbbd8023f9ac62b49d278ae1fa
hande-ozen <ozn.hande@gmail.com>
2025-10-13 01:37:51 +0200

    new template: power pop girls

---
+++ assets/js/templates/power-pop-girls.js
@@ -0,0 +1,27 @@
+//PowerPopGirls 11x15
+
+const powerPopGirlsConfig = {
+    name: "power-pop-girls",
+    width: 11,
+    height: 7,
+    mode: "dark",
+    colors: ["#FFDC4B", "#FFFFFF", "#89C8FF", "#0C1B68", "#000000", "#FEDAC4"]
+};
+
+const powerPopGirls = [
+    "", "#FFDC4B", "#FFDC4B", "", "", "", "", "",  "#FFDC4B", "#FFDC4B", "",
+    "#FFDC4B", "#FFDC4B", "#0C1B68", "#FFDC4B", "#FFDC4B", "#FFDC4B", "#FFDC4B", "#FFDC4B", "#0C1B68", "#FFDC4B", "#FFDC4B",
+    "", "", "#FFDC4B", "#FFDC4B", "#FFDC4B", "#FFDC4B", "#FFDC4B", "#FFDC4B", "#FFDC4B", "", "",
+    "", "", "#FFFFFF", "#89C8FF", "#000000", "#FEDAC4", "#000000", "#89C8FF", "#FFFFFF", "", "",
+    "", "", "#89C8FF", "#000000", "#FFFFFF", "#FEDAC4", "#FFFFFF", "#000000", "#89C8FF", "", "",
+    "", "", "#FFFFFF", "#89C8FF", "#000000", "#FEDAC4", "#000000", "#89C8FF", "#FFFFFF", "", "",
+    "", "", "", "#FEDAC4", "#FEDAC4", "#FEDAC4", "#FEDAC4", "#FEDAC4", "", "", "",
+]   
+
+if (templates) {
+    templates.push({
+        config: powerPopGirlsConfig,
+        pixels: powerPopGirls
+    });
+    console.log(powerPopGirlsConfig.name + " injected");
+} else console.log("couldn't inject " + powerPopGirlsConfig.name);
